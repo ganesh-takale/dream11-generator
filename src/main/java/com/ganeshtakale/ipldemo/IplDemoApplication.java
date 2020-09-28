b151610d9631cc6214c35875b56f31121b728df5
ganesh.takale <ganesh.takale@perennialsys.com>
2020-09-28 20:42:12 +0530

    first commit

---
+++ src/main/java/com/ganeshtakale/ipldemo/IplDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ganeshtakale.ipldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IplDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganeshtakale/ipldemo/IplDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshtakale.ipldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
