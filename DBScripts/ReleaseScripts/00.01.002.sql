6661f8f527fbc4dbaf5b2afc7681d00b95efcc8b
ganesh.takale <ganesh.takale@perennialsys.com>
2020-09-29 00:33:34 +0530

    new dbscript added

---
+++ DBScripts/ReleaseScripts/00.01.002.sql
@@ -0,0 +1,8 @@
+-- #################################################################
+-- ### Server version: 			5.7
+-- ### Date (DD-MM-YYYY): 		29/09/2020
+-- ### Developer Name: 			Ganesh Takale
+-- ### Comments:				alter table players
+-- #################################################################
+
+alter table players add column in_playing11 tinyint(1) default 0;
