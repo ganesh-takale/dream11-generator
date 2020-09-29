613cf2e41722a6c5676b331326180327ce63092f
ganesh.takale <ganesh.takale@perennialsys.com>
2020-09-29 15:37:00 +0530

    dbscipt updated

--- DBScripts/ReleaseScripts/00.01.002.sql
+++ DBScripts/ReleaseScripts/00.01.002.sql
@@ -6,3 +6,31 @@
 -- #################################################################
 
 alter table players add column in_playing11 tinyint(1) default 0;
+
+-- KXIP
+insert into players (name,credits,type,team_id) values
+('Mandeep Singh','8','BAT','8'),
+('Karun Nair','8.5','BAT','8'),
+('Sarfaraz Khan','8','BAT','8'),
+('Mayank Agarwal','9.5','BAT','8'),
+('Chris Gayle','9.5','BAT','8'),
+('Harpreet Brar','8','ALL','8'),
+('James Neesham','8.5','ALL','8'),
+('Tajinder Singh','7.5','ALL','8'),
+('Deepak Hooda','8','ALL','8'),
+('Glenn Maxwell','9','ALL','8'),
+('Krishnappa Gowtham','8.5','ALL','8'),
+('KL Rahul','10.5','WK','8'),
+('Nicholas Pooran','9','WK','8'),
+('Simran Singh','7.5','WK','8'),
+('Ishan Porel','8','BOW','8'),
+('Chris Jordan','8.5','BOW','8'),
+('Ravi Bishnoi','8','BOW','8'),
+('Arshdeep Singh','7.5','BOW','8'),
+('Mujeeb Ur Rahman ','9','BOW','8'),
+('Sheldon Cottrell ','8.5','BOW','8'),
+('Mohammed Shami','9','BOW','8'),
+('Darshan Nalkande','8','BOW','8'),
+('Murugan Ashwin','8','BOW','8'),
+('Jagadeesha Suchith','8','BOW','8'),
+('Hardus Viljoen','8','BOW','8');
