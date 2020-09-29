1b1291257121a46bd788d73944321683c207673a
ganesh.takale <ganesh.takale@perennialsys.com>
2020-09-29 10:20:23 +0530

    UI team list changes

--- ipl-demo-ui/src/TeamsComponent/teams-list.component.js
+++ ipl-demo-ui/src/TeamsComponent/teams-list.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TeamService from "../services/team.service";
+import PlayerService from "../services/player.service";
 import { Link } from "react-router-dom";
 
 export default class TeamsList extends Component {
@@ -10,11 +11,16 @@ export default class TeamsList extends Component {
         this.refreshList = this.refreshList.bind(this);
         this.setActiveTeam = this.setActiveTeam.bind(this);
         this.searchTitle = this.searchTitle.bind(this);
+        this.retrievePlayers = this.retrievePlayers.bind(this);
+        this.setActivePlayer = this.setActivePlayer.bind(this);
 
         this.state = {
             teams: [],
+            players:[],
             currentTeam: null,
+            currentPlayer: null,
             currentIndex: -1,
+            currentPlayerIndex: -1,
             searchTitle: ""
         };
     }
@@ -44,6 +50,19 @@ export default class TeamsList extends Component {
             });
     }
 
+    retrievePlayers(id) {
+        PlayerService.get(id)
+            .then(response => {
+                this.setState({
+                    players: response.data
+                });
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    }
+
     refreshList() {
         this.retrieveTeams();
         this.setState({
@@ -53,9 +72,17 @@ export default class TeamsList extends Component {
     }
 
     setActiveTeam(team, index) {
+        this.retrievePlayers(team.id);
         this.setState({
             currentTeam: team,
             currentIndex: index
+        });
+    }
+
+    setActivePlayer(player, index) {
+        this.setState({
+            currentPlayer: player,
+            currentPlayerIndex: index
         });
     }
 
@@ -63,7 +90,7 @@ export default class TeamsList extends Component {
         TeamService.findByTitle(this.state.searchTitle)
             .then(response => {
                 this.setState({
-                    tutorials: response.data
+                    teams: response.data
                 });
                 console.log(response.data);
             })
@@ -73,7 +100,7 @@ export default class TeamsList extends Component {
     }
 
     render() {
-        const { searchTitle, teams, currentTeam, currentIndex } = this.state;
+        const { searchTitle, teams, players, currentTeam, currentIndex, currentPlayerIndex } = this.state;
         return (
             <div className="list row">
                 <div className="col-md-8">
@@ -118,25 +145,22 @@ export default class TeamsList extends Component {
                 <div className="col-md-6">
                     {currentTeam ? (
                         <div>
-                            <h4>Team</h4>
-                            <div>
-                                <label>
-                                    <strong>Title:</strong>
-                                </label>{" "}
-                                {currentTeam.name}
-                            </div>
-                            <div>
-                                <label>
-                                    <strong>Description:</strong>
-                                </label>{" "}
-                                {currentTeam.code}
-                            </div>
-                            <Link
-                                to={"/players/" + currentTeam.id}
-                                className="badge badge-warning"
-                            >
-                                Edit
-                            </Link>
+                            <h4>{currentTeam.name}</h4>
+                            <ul className="list-group">
+                                {players &&
+                                players.map((player, index) => (
+                                    <li
+                                        className={
+                                            "list-group-item " +
+                                            (index === currentPlayerIndex ? "active" : "")
+                                        }
+                                        onClick={() => this.setActivePlayer(player, index)}
+                                        key={index}
+                                    >
+                                        {player.name} ({player.credits})
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     ) : (
                         <div>
