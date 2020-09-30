cf020a41f13d06cd0dc33e43b369fad9c5b39df8
ganesh.takale <ganesh.takale@perennialsys.com>
2020-09-30 10:29:51 +0530

    match service added

---
+++ src/main/java/com/ganeshtakale/ipldemo/model/Matches.java
@@ -0,0 +1,47 @@
+package com.ganeshtakale.ipldemo.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Entity
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Matches implements Serializable {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "date_time")
+	private Date dateTime;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Teams team1;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	private Teams team2;
+
+	@Column(name = "location")
+	private String location;
+
+	public Matches() {
+	}
+
+
+}
