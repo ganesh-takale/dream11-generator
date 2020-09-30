3844e2204b34c902ca40d0291b730578ea74ff24
ganesh.takale <ganesh.takale@perennialsys.com>
2020-09-30 15:24:05 +0530

    update player API issue fixes

--- src/main/java/com/ganeshtakale/ipldemo/controller/PlayersController.java
+++ src/main/java/com/ganeshtakale/ipldemo/controller/PlayersController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -42,7 +43,7 @@ public class PlayersController {
 	}
 
 	@PostMapping("/player/{playerId}")
-	public JSONObject getById(@PathVariable long playerId, @RequestBody Players player) {
-		return ps.updatePlayer(playerId,player);
+	public @ResponseBody String getById(@PathVariable long playerId, @RequestBody Players player) {
+		return ps.updatePlayer(playerId,player).toString();
 	}
 }
